3b8ba1accf727bfa5a1ac74cf9a84cd2e8e303d8
devashish gupta <devashishgupta20102000@gmail.com>
2024-07-11 10:45:14 +0530

    Update app.config.ts

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,4 +1,7 @@
 import process from "process"
+const role=localStorage.getItem('role')
+console.log(role);
+
 export type AppConfig = {
     apiPrefix: string
     authenticatedEntryPath: string
@@ -10,7 +13,7 @@ export type AppConfig = {
 
 const appConfig: AppConfig = {
     apiPrefix: 'https://colonelz-back.test.initz.run/v1/api/',
-    authenticatedEntryPath: '/app/crm/dashboard',
+    authenticatedEntryPath:(role==='Site Supervisor' || role==='Jr. Executive HR & Marketing')?'/app/crm/fileManager':'/app/crm/dashboard',
     unAuthenticatedEntryPath: '/sign-in',
     tourPath: '/app/account/kyc-form',
     locale: 'en',
